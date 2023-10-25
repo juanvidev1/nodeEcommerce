300bb7fedfc03518c853d3abff3fc31f6ddd301d
juanvidev <juanvidev1@gmail.com>
2023-10-24 21:09:21 -0500

    feat: Add a customers table to explore 1 to 1 relations

---
+++ db/models/customerModel.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const CUSTOMER_TABLE = 'customers';
+
+const CustomerSchema = {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'first_name'
+    },
+    lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name'
+    },
+    phone: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    isActive: {
+        defaultValue: true,
+        type: DataTypes.BOOLEAN
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+    }
+}
+
+class Customer extends Model
+{
+    static associate() {
+        // Modelos relacionados
+    }
+
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: CUSTOMER_TABLE,
+            modelName: 'Customer',
+            timestamps: true
+        }
+    }
+}
+
+module.exports = { CUSTOMER_TABLE, CustomerSchema, Customer };
--- db/models/index.js
+++ db/models/index.js
@@ -1,11 +1,13 @@
 const { User, UserSchema } = require('./userModel'); 
 const { Product, ProductSchema } = require('./productModel');
 const { Category, CategorySchema } = require('./categoryModel');
+const { Customer, CustomerSchema } = require('./customerModel');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
 }
 
 module.exports = setupModels;
